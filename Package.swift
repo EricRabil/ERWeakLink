3044f0709cba50daa35be11a28858cd263b69051
Eric Rabil <ericjrabil@gmail.com>
2022-07-03 18:19:22 -0400

    Support iOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ERWeakLink",
+    platforms: [.macOS(.v10_12), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
